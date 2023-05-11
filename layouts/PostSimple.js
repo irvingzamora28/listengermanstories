57ac10ccd55bc257ff46b9bd9f92b2bc841ef54a
Irving Zamora <irvingzamora28@gmail.com>
2023-05-11 00:22:33 -0600

    Add word count and reading time to post PostSimple

--- layouts/PostSimple.js
+++ layouts/PostSimple.js
@@ -8,7 +8,7 @@ import Comments from '@/components/comments'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 
 export default function PostLayout({ frontMatter, authorDetails, next, prev, children }) {
-  const { date, title } = frontMatter
+  const { date, title, readingTime } = frontMatter
 
   return (
     <SectionContainer>
@@ -28,39 +28,46 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
               </dl>
               <div>
                 <PageTitle>{title}</PageTitle>
+                <div className="flex justify-center gap-5">
+                  <span className="flex items-center gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                    </svg>
+                    {readingTime.words}
+                    words
+                  </span>
+                  <span className="flex items-center gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    {readingTime.text}{' '}
+                  </span>
+                </div>
               </div>
             </div>
           </header>
-          <div
-            className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0 "
-            style={{ gridTemplateRows: 'auto 1fr' }}
-          >
+          <div className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0 " style={{ gridTemplateRows: 'auto 1fr' }}>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
-              <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
+              <div className="prose max-w-none pb-8 pt-10 dark:prose-dark">{children} </div>
             </div>
             <Comments frontMatter={frontMatter} />
             <footer>
               <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
                 {prev && (
                   <div className="pt-4 xl:pt-8">
-                    <Link
-                      href={`/blog/${prev.slug}`}
-                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    >
-                      &larr; {prev.title}
+                    <Link href={`/blog/${prev.slug}`} className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                      &larr; {prev.title}{' '}
                     </Link>
                   </div>
                 )}
                 {next && (
                   <div className="pt-4 xl:pt-8">
-                    <Link
-                      href={`/blog/${next.slug}`}
-                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                    >
-                      {next.title} &rarr;
+                    <Link href={`/blog/${next.slug}`} className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                      {next.title}
+                      &rarr;
                     </Link>
                   </div>
-                )}
+                )}{' '}
               </div>
             </footer>
           </div>
