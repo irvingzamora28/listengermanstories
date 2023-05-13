fdc0f4679c56da1ca9923a4491c102e0d33e8619
Irving Zamora <irvingzamora28@gmail.com>
2023-05-12 23:14:59 -0600

    Make the TextToSpeechPlayer component work with larger texts by cutting up the text in chunks of 100 characters or when a dot is encountered.

--- components/TextToSpeechPlayer.js
+++ components/TextToSpeechPlayer.js
@@ -6,57 +6,65 @@ const TextToSpeechPlayer = ({ text }) => {
   const [isPaused, setIsPaused] = useState(false)
   const [isRepeating, setIsRepeating] = useState(false)
   const [isRestarting, setIsRestarting] = useState(false)
-
-  const utteranceRef = useRef(null)
+  const [utteranceQueue, setUtteranceQueue] = useState([])
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      // Create SpeechSynthesisUtterance instance only on the client-side
-      utteranceRef.current = new SpeechSynthesisUtterance(text)
-      utteranceRef.current.lang = 'de-DE'
+      const utterances = []
+      const regex = /.{1,100}(,|\.|$)|.{1,100}/g
+      const chunks = text.match(regex)
+      console.log(chunks)
+      chunks.forEach((chunk) => {
+        const utterance = new SpeechSynthesisUtterance(chunk)
+        utterance.lang = 'de-DE'
+        const voices = window.speechSynthesis.getVoices()
+        const selectedVoice = voices.find((voice) => voice.lang === 'de-DE')
+        if (selectedVoice) {
+          utterance.voice = selectedVoice
+        }
+        utterances.push(utterance)
+      })
+
+      setUtteranceQueue(utterances)
     }
   }, [text])
 
-  const handlePlay = () => {
-    utteranceRef.current.rate = 0.8
-    utteranceRef.current.pitch = 1
+  const speakNextChunk = () => {
     window.speechSynthesis.cancel()
-    setIsPlaying(true)
-
-    const voices = window.speechSynthesis.getVoices()
-    const selectedVoice = voices.find((voice) => voice.lang === 'de-DE')
-    if (selectedVoice) {
-      utteranceRef.current.voice = selectedVoice
+    console.log(`length: ${utteranceQueue.length}`)
+    if (utteranceQueue.length === 0) {
+      setIsPlaying(false)
+      setIsPaused(false)
+      return
     }
-    window.speechSynthesis.speak(utteranceRef.current)
-  }
 
-  const handlePause = () => {
-    setIsPaused(true)
-    window.speechSynthesis.pause()
-    setIsPlaying(false)
-  }
+    const utterance = utteranceQueue.shift()
+    utterance.rate = 0.8
+    utterance.pitch = 1
 
-  const handleResume = () => {
-    setIsPaused(false)
-    window.speechSynthesis.resume()
-    setIsPlaying(true)
+    utterance.onend = speakNextChunk
+    console.log(`play`, utterance)
+    window.speechSynthesis.speak(utterance)
   }
 
   const handlePlayPause = () => {
-    if (!isPlaying && !isPaused) handlePlay()
-    else if (isPaused) handleResume()
-    else handlePause()
-  }
-
-  const handleRepeat = () => {
-    setIsRepeating(!isRepeating)
+    if (!isPlaying && !isPaused) {
+      setIsPlaying(true)
+      speakNextChunk()
+    } else if (isPaused) {
+      setIsPaused(false)
+      window.speechSynthesis.resume()
+    } else {
+      setIsPaused(true)
+      window.speechSynthesis.pause()
+    }
   }
 
   const handleRestart = () => {
     window.speechSynthesis.cancel()
     setIsPlaying(false)
     setIsPaused(false)
+    setUtteranceQueue([])
     setIsRestarting(true)
   }
 
@@ -67,13 +75,6 @@ const TextToSpeechPlayer = ({ text }) => {
     }
   }, [isPlaying, isPaused, isRepeating, isRestarting])
 
-  useEffect(() => {
-    utteranceRef.current.onend = () => {
-      setIsPlaying(false)
-      setIsPaused(false)
-    }
-  }, [])
-
   return (
     <div className="flex items-center justify-center space-x-4 rounded-lg bg-green-200 p-4 shadow-lg hover:shadow-xl">
       <button onClick={handleRestart} className="rounded-full p-2 hover:bg-green-300 hover:text-white">
@@ -82,7 +83,7 @@ const TextToSpeechPlayer = ({ text }) => {
       <button onClick={handlePlayPause} className="rounded-full p-2 hover:bg-green-300 hover:text-white">
         {isPlaying ? <FiPauseCircle size={24} /> : <FiPlayCircle size={24} />}
       </button>
-      <button onClick={handleRepeat} className="rounded-full p-2 hover:bg-green-300 hover:text-white">
+      <button onClick={() => setIsRepeating(!isRepeating)} className="rounded-full p-2 hover:bg-green-300 hover:text-white">
         <FiRepeat size={24} className={` ${isRepeating && 'rounded-full text-green-600 hover:text-white'}`} />
       </button>
     </div>
