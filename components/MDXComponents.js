5dd2fb548dac0599d1ec5e722ad463e37079ca56
Irving Zamora <irvingzamora28@gmail.com>
2025-02-13 03:10:40 -0700

    feat: Adds TextToSpeechPlayer component to MDXComponents
    Integrates TextToSpeechPlayer component for accessibility and audio playback

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -6,12 +6,14 @@ import CustomLink from './Link'
 import TOCInline from './TOCInline'
 import Pre from './Pre'
 import { BlogNewsletterForm } from './NewsletterForm'
+import TextToSpeechPlayer from './TextToSpeechPlayer.js'
 
 export const MDXComponents = {
   Image,
   TOCInline,
   a: CustomLink,
   pre: Pre,
+  TextToSpeechPlayer: TextToSpeechPlayer,
   BlogNewsletterForm: BlogNewsletterForm,
   wrapper: ({ components, layout, ...rest }) => {
     const Layout = require(`../layouts/${layout}`).default
